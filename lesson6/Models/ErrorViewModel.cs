76d44ff496861f9cac6f749fa9d019aeed05c6f3
eladyek <eladyek@gmail.com>
2019-04-01 21:25:57 +0300

    Add project files.

---
+++ lesson6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lesson6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
